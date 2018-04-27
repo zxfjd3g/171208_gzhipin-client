2698d8286ba831f221265f93f553e3d8ea33217b
zxfjd3g <258147149@qq.com>
2018-04-27 13:47:34 +0800

    init app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+入口JS
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Button} from 'antd-mobile'
+
+ReactDOM.render(<Button type="primary">Test</Button>, document.getElementById('root'))

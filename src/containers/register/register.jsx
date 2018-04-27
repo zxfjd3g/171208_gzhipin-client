9e64818a49b91a6579dfa5417f87d9a675dc4f71
zxfjd3g <258147149@qq.com>
2018-04-28 07:31:14 +0800

    register组件/总结

--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -48,18 +48,18 @@ export default class Register extends Component {
         <WingBlank>
           <List>
             <WhiteSpace/>
-            <InputItem onChange={val => {this.handleChange('username', val)}}>用户名:</InputItem>
+            <InputItem placeholder='请输入用户名' onChange={val => {this.handleChange('username', val)}}>用户名:</InputItem>
             <WhiteSpace/>
-            <InputItem type="password" onChange={val => {this.handleChange('password', val)}}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
+            <InputItem placeholder='请输入密码' type="password" onChange={val => {this.handleChange('password', val)}}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
             <WhiteSpace/>
-            <InputItem type="password" onChange={val => {this.handleChange('password2', val)}}>确认密码:</InputItem>
+            <InputItem placeholder='请输入确认密码' type="password" onChange={val => {this.handleChange('password2', val)}}>确认密码:</InputItem>
             <WhiteSpace/>
             <ListItem>
               <span>用户类型:</span>
               &nbsp;&nbsp;&nbsp;
               <Radio checked={type==='dashen'} onChange={() => this.handleChange('type', 'dashen')}>大神</Radio>
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <Radio checked={type==='laoban'}  onChange={() => this.handleChange('type', 'laoban')}>老板</Radio>
+              <Radio checked={type==='laoban'}  onClick={() => this.handleChange('type', 'laoban')}>老板</Radio>
             </ListItem>
             <WhiteSpace/>
             <Button type='primary' onClick={this.register}>注&nbsp;&nbsp;&nbsp;册</Button>

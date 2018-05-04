bc8002ab53b14ca76accb6e5899d23108dfb1008
zxfjd3g <258147149@qq.com>
2018-05-04 09:44:41 +0800

    socketio

--- src/test/socketio_test.js
+++ src/test/socketio_test.js
@@ -1,14 +1,12 @@
-// 引入客户端io
 import io from 'socket.io-client'
 
-// 连接服务器, 得到代表连接的socket对象
+// 连接服务器, 得到与服务器的连接对象
 const socket = io('ws://localhost:4000')
 
-// 绑定'receiveMessage'的监听, 来接收服务器发送的消息
+// 发送消息
+socket.emit('sendMsg', {name: 'abc'})
+console.log('客户端向服务器发消息', {name: 'abc'})
+// 绑定监听, 接收服务器发送的消息
 socket.on('receiveMsg', function (data) {
-  console.log('浏览器端接收到消息:', data)
+  console.log('客户端接收服务器发送的消息', data)
 })
-
-// 向服务器发送消息
-socket.emit('sendMsg', {name: 'Tom', date: Date.now()})
-console.log('浏览器端向服务器发送消息:', {name: 'Tom', date: Date.now()})
